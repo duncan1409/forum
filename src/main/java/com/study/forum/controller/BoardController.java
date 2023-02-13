fdf15df3c24fd6043ee201a2b4f37df0fc412fdf
duncan1409 <duncansITworld@gmail.com>
2023-02-13 14:57:32 +0900

    게시판 작성, 수정, 삭제 구현

--- src/main/java/com/study/forum/controller/BoardController.java
+++ src/main/java/com/study/forum/controller/BoardController.java
@@ -2,7 +2,9 @@ package com.study.forum.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.study.forum.entity.Board;
@@ -10,9 +12,11 @@ import com.study.forum.service.BoardService;
 
 @Controller
 public class BoardController {
+
 	@Autowired
 	private BoardService boardService;
 
+	// 글 작성 페이지
 	@GetMapping("/board/write") // localhost:8090/board/write
 	public String boardWriteForm() {
 		return "boardwrite";
@@ -24,4 +28,45 @@ public class BoardController {
 		boardService.write(board);
 		return "";
 	}
+
+	@GetMapping("/board/list")
+	public String boardList(Model model) {
+		model.addAttribute("list", boardService.boardList());
+		return "boardlist";
+	}
+
+	// 특정 게시글 페이지 (GET 방식 처리): localhost:8090/board/view?id=1
+	@GetMapping("/board/view")
+	public String boardView(Model model, Integer id) {
+		model.addAttribute("board", boardService.boardView(id));
+		return "boardview";
+	}
+
+	// 글 삭제 페이지: 바로 redirect->list
+	@GetMapping("/board/delete")
+	public String boardDelete(Integer id) {
+		boardService.boardDelete(id);
+
+		return "redirect:/board/list";
+	}
+
+	// 글 수정 페이지 라우터 id
+	@GetMapping("/board/modify/{id}")
+	public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+		model.addAttribute("board", boardService.boardView(id));
+		return "boardmodify";
+	}
+
+	@PostMapping("board/update/{id}")
+	public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+		Board boardTemp = boardService.boardView(id);
+		boardTemp.setTitle(board.getTitle());
+		boardTemp.setContent(board.getContent());
+
+		boardService.write(boardTemp);
+
+		return "redirect:/board/list";
+	}
+
 }
--- src/main/java/com/study/forum/service/BoardService.java
+++ src/main/java/com/study/forum/service/BoardService.java
@@ -1,10 +1,11 @@
 package com.study.forum.service;
 
+import com.study.forum.entity.Board;
+import com.study.forum.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.study.forum.entity.Board;
-import com.study.forum.repository.BoardRepository;
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -12,7 +13,24 @@ public class BoardService {
 	@Autowired
 	private BoardRepository boardRepository;
 
+	// 글 작성 메소드
 	public void write(Board board) {
 		boardRepository.save(board);
 	}
+
+	// 게시글 리스트 불러오기 메소드
+	public List<Board> boardList() {
+		return boardRepository.findAll();
+	}
+
+	// 특정 게시글 불러오기 메소드
+	public Board boardView(Integer id) {
+		return boardRepository.findById(id).get();
+	}
+
+	// 특정 게시글 삭제
+	public void boardDelete(Integer id) {
+		boardRepository.deleteById(id);
+	}
+
 }
